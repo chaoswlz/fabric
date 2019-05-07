115e835a5ce9318afa533a28db525322f1eb8ede
chaoswlz <wulize1994@gmail.com>
2019-05-07 14:27:48 -0700

    add bash

---
+++ multipeer/channel-artifacts/auto.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# Copyright IBM Corp. All Rights Reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+export ORDERER_CA=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
+
+while : do 
+    for ((i=1; i<=100; i++)); do
+        ((Num=$i %2))
+        if ["$Num" == 0]; then
+			peer chaincode invoke --tls --cafile ${ORDERER_CA} -C mychannel -n mycc -c '{"Args":["invoke","b","a","20"]}'
+		fi
+        if ["$Num" != 0]; then
+			peer chaincode invoke --tls --cafile ${ORDERER_CA} -C mychannel -n mycc -c '{"Args":["invoke","a","b","20"]}'
+		fi
+    done
+    sleep 1
+done
